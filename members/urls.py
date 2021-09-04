4ce64691eab71ac9ab71fc259a042e452ef8e218
Juan <garcij07@myunitec.ac.nz>
2021-09-05 05:27:07 +1200

    added basic templates

--- members/urls.py
+++ members/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('settings/', EditUserView.as_view(), name='settings'),
 
     path('create_rep/', CreateRepView.as_view(), name='create_rep'),
-    path('<int:pk>/rep/', DisplayRepView.as_view(), name='show_profile'),
-    path('<int:pk>/edit_rep/', EditRepView.as_view(), name='edit_profile_page'),
+    path('<int:pk>/rep/', DisplayRepView.as_view(), name='show_rep_profile'),
+    path('<int:pk>/edit_rep/', EditRepView.as_view(), name='edit_rep_profile'),
 
     path('create_supplier/', CreateSupplierView.as_view(), name='create_supplier'),
     path('<int:pk>/supplier/', DisplaySupplierView.as_view(), name='show_supplier'),
