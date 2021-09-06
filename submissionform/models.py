b62dc6fe54dd60b1eafb69d7e52e5411cb12801e
Juan <garcij07@myunitec.ac.nz>
2021-09-06 23:41:38 +1200

    made form have correct components

--- submissionform/models.py
+++ submissionform/models.py
@@ -23,18 +23,30 @@ class Job(models.Model):
     site_address = models.TextField()
     job_location = models.TextField()
 
+    JOB_LOCATIONS = [
+
+        ('AUc', 'Auckland'),
+        ('WAI', 'Waikato'),
+    ]
+
+    job_location = models.CharField(
+        choices=JOB_LOCATIONS,
+        default=0,
+        max_length=3
+    )
+
     # Client info
-    client = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=255)
+    client_name = models.CharField(max_length=255)
+    client_phone = models.CharField(max_length=255)
+    client_mobile = models.CharField(max_length=255)
     rep = models.ForeignKey(Rep, on_delete=models.CASCADE)
     logged_date = models.DateField()
     client_address = models.TextField()
-
+    client_email = models.EmailField()
 
     # Detailer info
-    name = models.CharField(max_length=255)
-    contact_number = models.CharField(max_length=255)
+    detailer_name = models.CharField(max_length=255)
+    detailer_contact_number = models.CharField(max_length=255)
 
     # email list info
     email_skip = models.BooleanField()
@@ -45,14 +57,49 @@ class Job(models.Model):
     email_to = models.EmailField()
     suppliers = models.ManyToManyField(Supplier)
 
+    # checkboxes
+    # section 1
+    truss_layout_ps1_only = models.BooleanField()
+    full_buildable_layouts = models.BooleanField()
+    detailing = models.BooleanField()
+    steel_reinforcing = models.BooleanField()
+    pre_bent_r_steel = models.BooleanField()
+    rib_raft = models.BooleanField()
+    masonry = models.BooleanField()
+    concrete = models.BooleanField()
+    # section 2
+    pre_nail = models.BooleanField()
+    trusses = models.BooleanField()
+    rafters = models.BooleanField()
+    roof_pack = models.BooleanField()
+    random = models.BooleanField()
+    sub_floor = models.BooleanField()
+    mid_floor = models.BooleanField()
+    deck = models.BooleanField()
+    j_frame = models.BooleanField()
+    # _section_3
+    cladding = models.BooleanField()
+    insulation_only = models.BooleanField()
+    insulation_installed = models.BooleanField()
+    plasterboard = models.BooleanField()
+    plasterboard_dts = models.BooleanField()
+    internal_linings = models.BooleanField()
+    internal_doors = models.BooleanField()
+    # _section_4
+    timber_fascia = models.BooleanField()
+    marley_gutter_dp = models.BooleanField()
+    roofing = models.BooleanField()
+    aluminium_joinery = models.BooleanField()
+    pergola = models.BooleanField()
+    retaining_wall = models.BooleanField()
 
     WINDZONES = [
-        (0, 'NONE'),
-        (1, 'LOW'),
-        (2, 'MEDIUM'),
-        (3, 'HIGH'),
-        (4, 'VERY_HIGH'),
-        (5, 'EXTRA_HIGH'),
+        (0, 'No Wind'),
+        (1, 'Low Wind'),
+        (2, 'Medium Wind'),
+        (3, 'High Wind'),
+        (4, 'Very High Wind'),
+        (5, 'Extra High Wind'),
     ]
     wind_zone = models.IntegerField(
         choices=WINDZONES,
@@ -73,16 +120,16 @@ class Job(models.Model):
     )
 
     # Note product required for the following
-    interior_doors_notes = models.BooleanField()
-    door_hardware_notes = models.BooleanField()
-    scotia_notes = models.BooleanField()
-    skirting_notes = models.BooleanField()
-    architraves_notes = models.BooleanField()
-    fascia_type_notes = models.BooleanField()
-    ceiling_batten_notes = models.BooleanField()
-    roof_material_notes = models.BooleanField()
-    truss_penetrations_notes = models.BooleanField()
-    special_trusses_notes = models.BooleanField()
+    interior_doors_notes = models.CharField(max_length=255)
+    door_hardware_notes = models.CharField(max_length=255)
+    scotia_notes = models.CharField(max_length=255)
+    skirting_notes = models.CharField(max_length=255)
+    architraves_notes = models.CharField(max_length=255)
+    fascia_type_notes = models.CharField(max_length=255)
+    ceiling_batten_notes = models.CharField(max_length=255)
+    roof_material_notes = models.CharField(max_length=255)
+    truss_penetrations_notes = models.CharField(max_length=255)
+    special_trusses_notes = models.CharField(max_length=255)
 
     alteration_notes = models.TextField()
 
@@ -94,12 +141,12 @@ class Job(models.Model):
     dwg_floor_plan_file = models.FileField(null=True, blank=True, upload_to="images/dog-profiles")
 
     # information in plans
-    dimensioned_floor_on_plan = models.CharField(max_length=255)
-    elevations_on_plan = models.CharField(max_length=255)
-    cross_section_on_plan = models.CharField(max_length=255)
-    roof_pitch_on_plan = models.CharField(max_length=255)
-    electrical_plan_on_plan = models.CharField(max_length=255)
-    rafters_design_and_sizing_on_plan = models.CharField(max_length=255)
+    dimensioned_floor_on_plan = models.BooleanField()
+    elevations_on_plan = models.BooleanField()
+    cross_section_on_plan = models.BooleanField()
+    roof_pitch_on_plan = models.BooleanField()
+    electrical_plan_on_plan = models.BooleanField()
+    rafters_design_and_sizing_on_plan = models.BooleanField()
 
     def __str__(self):
         return self.name + " " + self.rep.user.get_full_name()
